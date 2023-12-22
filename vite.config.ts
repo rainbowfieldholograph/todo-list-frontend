b682d741ebba55d31b9151225061f7bc850c7758
rainbowfieldholograph <somebleed@tutanota.com>
2023-12-22 17:33:12 +0300

    bump vite 4 version; update: vite conifg, eslint and tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
+
+import path from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), tsConfigPaths()],
+	resolve: {
+		alias: {
+			'~': path.resolve(__dirname, './src'),
+		},
+	},
 });
