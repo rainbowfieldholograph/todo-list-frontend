c086fd0053456ac0dbfad06d00e314b9048c4a6d
rainbowfieldholograph <somebleed@tutanota.com>
2023-12-28 14:18:14 +0300

    add browserslist; add postcss prefixer

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
 import react from '@vitejs/plugin-react';
+import autoprefixer from 'autoprefixer';
 import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 import path from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	build: {
+		target: ['es2015', 'chrome58', 'firefox78', 'safari14'],
+	},
 	plugins: [react(), tsConfigPaths()],
+	css: {
+		postcss: { plugins: [autoprefixer()] },
+	},
 	resolve: {
-		alias: {
-			'~': path.resolve(__dirname, './src'),
-		},
+		alias: { '~': path.resolve(__dirname, './src') },
 	},
 });
