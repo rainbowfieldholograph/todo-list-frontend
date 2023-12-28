e09a3516778cf8e3185f9889831aa892070787fe
rainbowfieldholograph <somebleed@tutanota.com>
2023-12-28 12:18:54 +0300

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	build: {
+		target: ['es2015', 'chrome58', 'firefox78', 'safari14'],
+	},
 	plugins: [react(), tsConfigPaths()],
 	resolve: {
 		alias: {
